ae183e5073b82e5a949b0467e6558e5822a3958c
Lingrode <grimaloilya@gmail.com>
2024-06-05 23:20:36 -0600

    getting a list of users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
+// Here is a list of users: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Error getting users");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <Users />
+      <Users items={users} isLoading={isLoading} />
       {/* <Success /> */}
     </div>
   );
